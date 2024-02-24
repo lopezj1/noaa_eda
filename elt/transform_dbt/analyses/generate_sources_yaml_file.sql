7397d56b52f8a16e532fe4b21f64397d6a7a884e
lopezj1 <lopezjosh23@gmail.com>
2024-02-24 17:29:43 -0500

    updated sources yaml file for docs generation

--- elt/transform_dbt/analyses/generate_sources_yaml_file.sql
+++ elt/transform_dbt/analyses/generate_sources_yaml_file.sql
@@ -1,7 +1,9 @@
 {{ print('\nGenerate Source YAML File') }}
 {{ print('---------------') }}
+
 {{ codegen.generate_source(schema_name= 'raw', 
                             database_name= 'noaa_dw',
+                            table_names = ["catch","size","trip"],
                             generate_columns= true,
                             include_descriptions= true,
                             include_database= true,

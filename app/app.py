d46b5561c3a9858607205f2e08c0c299a87997ae
josh <lopezjosh23@gmail.com>
2024-08-06 17:46:21 +0000

    updated visualizations

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from duckdb_connection import DuckDBConnection
 import streamlit as st
 from pathlib import Path
 import plotly.express as px
+import plotly.graph_objects as go
 
 #configs
 st.set_page_config(
@@ -27,7 +28,8 @@ st.divider()
 df = conn.query(f"""select 
                 min(trip_year), 
                 max(trip_year), 
-                count(*) 
+                count(*),
+                cast(sum(total_number_fish_caught) as int)
                 from {SCHEMA}.trip_details
                 --where species_common_name in 
                 --(select species_common_name from {SCHEMA}.top_species)
@@ -35,17 +37,61 @@ df = conn.query(f"""select
 start_year = df.iat[0,0]
 end_year = df.iat[0,1]
 total_trips = df.iat[0,2]
+total_fish = df.iat[0,3]
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     st.metric(label="Date Range", value=f"{start_year}-{end_year}")
 with col2:
-    st.metric(label="Total Fishing Trips", value=f"{total_trips:,}")
+    st.metric(label="Total Number of Fishing Trips", value=f"{total_trips:,}")
+with col3:
+    st.metric(label="Total Number of Fish Caught", value=f"{total_fish:,}")
 
-tab1, tab2 = st.tabs(["Summary Statistics", "Historical Trends"])
+tab1, tab2, tab3 = st.tabs(["Overview", "Stats", "Trends"])
 with tab1:
     # DRY up df transforms
-    with st.expander("Catch Rate for Top 10 Targeted Species by US Region"):
+    with st.expander("Run Chart of Trips & Catches", expanded=True):
+        df = conn.query(f"""
+                        select 
+                        trip_year, 
+                        count(*) as total_trips,
+                        cast(sum(total_number_fish_caught) as int) as total_fish
+                        from {SCHEMA}.trip_details
+                        group by trip_year
+                        order by trip_year asc
+                        """)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df['trip_year'], y=df['total_trips'],
+                            mode='lines+markers',
+                            name='# Trips'))
+        fig.add_trace(go.Scatter(x=df['trip_year'], y=df['total_fish'],
+                            mode='lines+markers',
+                            name='# Fish Caught'))
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+    with st.expander("Treemap of Catches", expanded=True):
+        df = conn.query(f"""
+                        select 
+                        us_region,    
+                        fishing_season, 
+                        fishing_method_uncollapsed,
+                        species_common_name,
+                        cast(total_number_fish_caught as int) as total_fish
+                        from {SCHEMA}.trip_details
+                        """)
+        df = df.dropna() #drop non-leaves rows
+        fig = px.treemap(df, path=[px.Constant("all"), 
+                                   'us_region', 
+                                   'fishing_season', 
+                                   'fishing_method_uncollapsed',
+                                   'species_common_name'], 
+                                   values='total_fish')
+        fig.update_traces(root_color="lightgrey")
+        fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+with tab2:
+    with st.expander("Catch Rate for Top 10 Targeted Species by US Region", expanded=True):
         df = conn.query(f"select * from {SCHEMA}.region_catches")
         df = df.rename(columns={"species_common_name": "Species Common Name"})
         df = df.set_index("Species Common Name")
@@ -53,7 +99,7 @@ with tab1:
         keys = df.columns.tolist()
         st.dataframe(df.style.format('{:.2f}%').highlight_max(axis=1))
 
-    with st.expander("Catch Rate for Top 10 Targeted Species by Fishing Season"):
+    with st.expander("Catch Rate for Top 10 Targeted Species by Fishing Season", expanded=True):
         df = conn.query(f"select * from {SCHEMA}.season_catches")
         df = df.rename(columns={"species_common_name": "Species Common Name"})
         df = df.set_index("Species Common Name")
@@ -67,7 +113,7 @@ with tab1:
         df = df.select_dtypes(exclude=['datetime', 'object']) * 100
         st.dataframe(df.style.format('{:.2f}%').highlight_max(axis=1))
 
-with tab2:
+with tab3:
     df = conn.query(f"""select * from {SCHEMA}.trip_details 
                         where species_common_name
                         in (
@@ -101,30 +147,26 @@ with tab2:
         col1, col2, col3 = st.columns(3)
         with col1:
             x_axis_value = st.selectbox(label='Select X Axis',
-                                        index=2,
-                                        options=['fish_weight_lbs',
-                                                'fish_weight_kg', 
-                                                'fish_length_in',
-                                                'fish_length_cm',
+                                        index=0,
+                                        options=['total_length_fish_harvested_mm',
+                                                'total_weight_fish_harvested_kg', 
                                                 'trip_fishing_effort_hours',
                                                 'number_of_outings_in_last_2_months',
-                                                'number_of_outings_in_last_year',
-                                                'total_number_fish_caught'])
+                                                'number_of_outings_in_last_year'
+                                                ])
         with col2:
             y_axis_value = st.selectbox(label='Select Y Axis',
-                                        index=0,
-                                        options=['fish_weight_lbs',
-                                                'fish_weight_kg', 
-                                                'fish_length_in',
-                                                'fish_length_cm',
+                                        index=1,
+                                        options=['total_length_fish_harvested_mm',
+                                                'total_weight_fish_harvested_kg', 
                                                 'trip_fishing_effort_hours',
                                                 'number_of_outings_in_last_2_months',
-                                                'number_of_outings_in_last_year',
-                                                'total_number_fish_caught'])
+                                                'number_of_outings_in_last_year'
+                                                ])
         with col3:
             size_value = st.selectbox(label='Select Size Variable',
                                         index=0,
-                                        options=[])
+                                        options=['total_number_fish_caught'])
             
     fig = px.scatter(
         df.query('us_region in @region_filter & fishing_season in @season_filter & fishing_method_uncollapsed in @method_filter'),

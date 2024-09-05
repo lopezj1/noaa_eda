9bb7fb539e62bbc8d8fb6853cac1cdc66f8a602e
josh <lopezjosh23gmail.com>
2024-09-05 14:49:48 +0000

    update prefect version due to bug with griffe, using prefect worker instead of agent (deprecated), and updated prefect api url so can access prefect server via ssh tunnel

--- prefect_flows/prefect_startup.sh
+++ prefect_flows/prefect_startup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+prefect config set PREFECT_SERVER_API_HOST="0.0.0.0" &
+
+sleep 5
+
+prefect config set PREFECT_API_URL="http://0.0.0.0:4200/api" &
+
+sleep 5
 
 prefect server start &
 
 sleep 30
 
-prefect agent start --pool 'default-agent-pool' &
+prefect worker start -p 'my-pool' &
 
 sleep 30
 
